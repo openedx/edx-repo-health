6b7c618aa3b34dc6b8299032edad1091a8bb8500
jinder1s <msingh@edx.org>
2020-04-14 14:15:22 -0400

    Adding script to run things
    This script is still super WIP

---
+++ repo_health_dashboard/scripts/run1.sh
@@ -0,0 +1,29 @@
+repo_names=(
+'edx-analytics-data-api'
+'pytest-repo-health'
+'edx-platform'
+'warehouse-transforms'
+'edx-val'
+'edx-internal'
+'course-discovery'
+'terraform'
+'edx-analytics-dashboard'
+'edx-platform-private'
+'sphinxcontrib-openapi'
+'api-manager'
+'RateXBlock'
+'py-opt-cli'
+'stylelint-config-edx'
+'frontend-config-edx'
+'asym-crypto-yaml'
+'edx-user-state-client'
+'edx-postman-config'
+'journals-frontend'
+)
+touch tmp.txt
+for t in ${repo_names[@]}; do
+    git_url="git@github.com:edx/${t}.git"
+    git clone $git_url
+    pytest --repo-health --repo-health-path ../../edx-repo-health --repo-path $t --output-path ${t}_repo_health.yaml --noconftest -v -c tmp.txt
+    rm -rf $t
+done

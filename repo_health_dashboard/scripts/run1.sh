309d5c82e4a2b522c24dc12b07328ad1d6b3574d
jinder1s <msingh@edx.org>
2020-04-15 15:28:53 -0400

    Added argument to bash script

--- repo_health_dashboard/scripts/run1.sh
+++ repo_health_dashboard/scripts/run1.sh
@@ -1,3 +1,4 @@
+# the first argument for script is the location of edx-repo-health checks
 repo_names=(
 'edx-analytics-data-api'
 'pytest-repo-health'
@@ -24,6 +25,6 @@ touch tmp.txt
 for t in ${repo_names[@]}; do
     git_url="git@github.com:edx/${t}.git"
     git clone $git_url
-    pytest --repo-health --repo-health-path ../../edx-repo-health --repo-path $t --output-path ${t}_repo_health.yaml --noconftest -v -c tmp.txt
+    pytest --repo-health --repo-health-path $1 --repo-path $t --output-path ${t}_repo_health.yaml --noconftest -v -c tmp.txt
     rm -rf $t
 done

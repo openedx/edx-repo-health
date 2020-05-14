f16b75caf2a0ac85cd87557e5dd8d39d42317dff
Eric Herrera <eric.herrera@edunext.co>
2020-05-13 21:22:23 -0500

    Remove unsupported python versions. Add openedx.yaml.

--- setup.py
+++ setup.py
@@ -78,8 +78,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     entry_points={

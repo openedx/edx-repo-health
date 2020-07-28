c0b62dadcf41497da965b92cfd27d551646ce2e3
Jeremy Bowman <jbowman@edx.org>
2020-07-28 16:10:55 -0400

    Drop Python 3.5 support

--- setup.py
+++ setup.py
@@ -77,7 +77,8 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     entry_points={

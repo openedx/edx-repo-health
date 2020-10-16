ec38b685d466801a1fb518603ac9d9536dc01857
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-10-16 16:50:15 +0500

    BOM-1980 Adding a check to see if the repo is integrated with travis-ci.org or .com

---
+++ repo_health/check_travis_integration.py
@@ -0,0 +1,29 @@
+"""
+Checks repository is on travis.org or .com
+"""
+import requests
+from pytest_repo_health import add_key_to_metadata, health_metadata
+import json
+
+module_dict_key = "travis_ci"
+
+
+@add_key_to_metadata(module_dict_key)
+def check_travis_integration(all_results, git_origin_url):
+    """
+    Checks repository integrated with travis-ci.org or travis-ci.com
+    """
+
+    # by default assume its not on .com
+    all_results[module_dict_key]['active_on_com'] = False
+    all_results[module_dict_key]['active_on_org'] = True
+
+    link = git_origin_url.replace('git@github.com:edx/', '').replace('.git', '')  # picking repo name
+    resp = requests.get(
+        url='https://api.travis-ci.org/repo/edx%2F{link}'.format(link=link),
+        headers={'Travis-API-Version': '3'}
+    )
+
+    if resp.status_code == 200 and json.loads(resp.content)['migration_status'] == 'migrated':
+            all_results[module_dict_key]['active_on_com'] = True
+            all_results[module_dict_key]['active_on_org'] = False

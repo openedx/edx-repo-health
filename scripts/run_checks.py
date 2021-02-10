ea6906a9297edd8ca6d802edd382b0bf678bcec0
jinder1s <msingh@edx.org>
2021-02-10 09:23:04 -0500

    fix: remove unnecessary -c flag
    This did not seem necessary locally, though it did not cause any harms to have it there

--- scripts/run_checks.py
+++ scripts/run_checks.py
@@ -12,7 +12,7 @@ def main():
     Initiates pytest in repo-health mode
     """
     checks_dir = pathlib.Path(__file__).parent.parent.absolute()
-    pytest.main(["-c", "<()", "--noconftest", "--repo-health", "--repo-health-path", str(checks_dir)])
+    pytest.main(["--noconftest", "--repo-health", "--repo-health-path", str(checks_dir)])
 
 
 if __name__ == "__main__":

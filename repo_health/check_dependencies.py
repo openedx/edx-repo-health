6abce9b1a1a5a774de3887dd0aac251e5436c955
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-04-20 15:56:14 +0500

    Revert "refactor: converting dependencies list into json."

--- repo_health/check_dependencies.py
+++ repo_health/check_dependencies.py
@@ -1,14 +1,12 @@
 """
  Checks which are the dependencies of the repo
 """
-import json
-import os
 import re
 from pathlib import Path
+import os
 
 import pytest
 from pytest_repo_health import health_metadata
-
 from repo_health import get_file_lines
 
 module_dict_key = "dependencies"
@@ -19,8 +17,8 @@ def fixture_req_packages(repo_path):
     """
     Fixture containing the text content of req_files
     """
-    pypi_packages = {}
-    github_packages = {}
+    pypi_packages = []
+    github_packages = []
     files = [str(file) for file in Path(os.path.join(repo_path, "requirements")).rglob('*.txt')]
     constraints_files = ("constraints.txt", "pins.txt")
     requirement_files = [file for file in files if not file.endswith(constraints_files)]
@@ -28,20 +26,12 @@ def fixture_req_packages(repo_path):
         lines = get_file_lines(file_path)
         stripped_lines = [re.sub(r' +#.*', "", line).replace('-e ', "")
                           for line in lines if line and not line.startswith("#")]
-
-        for line in stripped_lines:
-            if re.match(r'^git\+.*', line):
-                key_val = line.split('==')
-                github_packages.update({key_val[0]: key_val[1]})
-
-        for line in stripped_lines:
-            if line not in github_packages and "==" in line:
-                key_val = line.split('==')
-                pypi_packages.update({key_val[0]: key_val[1]})
+        github_packages.extend([line for line in stripped_lines if re.match(r'^git\+.*', line)])
+        pypi_packages.extend([line for line in stripped_lines if line not in github_packages and "==" in line])
 
     return {
-        "pypi": json.dumps(pypi_packages),
-        "github": json.dumps(github_packages),
+        "pypi": list(set(pypi_packages)),
+        "github": list(set(github_packages)),
     }
 
 

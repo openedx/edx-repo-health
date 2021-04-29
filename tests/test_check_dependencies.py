54ab725c6fc2ec57692d0e775286277cbf0c4b02
Aarif <MrAarif@outlook.com>
2021-04-29 17:09:47 +0500

    fix: fixed the js dependencies output (#154)

--- tests/test_check_dependencies.py
+++ tests/test_check_dependencies.py
@@ -1,6 +1,6 @@
 import os
 
-from repo_health.check_dependencies import DependencyReader, get_dependencies
+from repo_health.check_dependencies import get_dependencies
 
 
 def get_repo_path(repo_name):
@@ -13,21 +13,24 @@ def test_python_js_repo_dependency_check():
     dependencies = get_dependencies(repo_path)
 
     assert 'fs==2.0.18' in dependencies.get("pypi").get("list")
-    assert "react-redux" in dependencies.get("js").get("list").keys()
+    assert "react-redux" in dependencies.get("js").get("list")
     assert dependencies.get("count") == 350
     assert dependencies.get("pypi").get("count") == 299
     assert dependencies.get("github").get("count") == 14
-    assert dependencies.get("js").get("count") == 37
+    assert dependencies.get("js").get("count") == 26
 
 
 def test_js_repo_dependency_check():
     repo_path = get_repo_path('fake_repos/js_repo')
     dependencies = get_dependencies(repo_path)
 
-    assert 'core-js' in dependencies.get("js").get("list").keys()
+    assert 'core-js' in dependencies.get("js").get("list")
+    assert 'jest' in dependencies.get("js.dev").get("list")
+
     assert dependencies.get("count") == 37
-    assert len(dependencies.get("js").get("list")) == 26
-    assert len(dependencies.get("js").get("dev.List")) == 11
+    assert dependencies.get("js").get("count") == 26
+    assert dependencies.get("js.dev").get("count") == 11
+
     assert dependencies.get("pypi").get("count") == 0
 
 

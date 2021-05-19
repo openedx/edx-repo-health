e55625aeda8f5955591fcaeaa0f2d5dfc2e96af0
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-05-19 16:49:10 +0500

    Add: Addig new check to get dockerfile apt-get install or update content. Also apt-get.txt content.
    BOM-2537

--- repo_health/check_ubuntufiles.py
+++ repo_health/check_ubuntufiles.py
@@ -8,6 +8,9 @@ import pytest
 from pytest_repo_health import health_metadata
 
 from repo_health import read_docker_file
+from pathlib import Path
+from repo_health import get_file_lines
+
 
 module_dict_key = "ubuntu_packages"
 
@@ -46,6 +49,23 @@ def get_docker_file_content(repo_path):
     return " ".join(value)
 
 
+def get_apt_get_txt(repo_path):
+    """
+   entry point to parse docker file and do cleaning.
+   @param repo_path:
+   @return: json data
+   """
+    full_path = os.path.join(repo_path, 'apt-packages.txt')
+    # check files on root or in requirements folder
+    content = get_file_lines(full_path)
+    if not content:
+        files = [str(file) for file in Path(os.path.join(repo_path, "requirements")).rglob('apt-packages.txt')]
+        if files:
+            content = get_file_lines(files[0])  # only one file will exists
+
+    return content
+
+
 def clean_data(content):
     """
     :param content:
@@ -58,7 +78,11 @@ def clean_data(content):
 @pytest.fixture(name='content')
 def fixture_ubuntu_content(repo_path):
     """Fixture containing the text content of dockerfile"""
-    return get_docker_file_content(repo_path)
+
+    return {
+        'docker_packages': get_docker_file_content(repo_path),
+        'apt_get_packages': get_apt_get_txt(repo_path)
+    }
 
 
 @health_metadata(

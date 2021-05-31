8e97c4e5d95654e296cbd62e52ca74ec638d5352
Jawayria <jawayriahashmi@gmail.com>
2021-05-31 20:01:37 +0500

    test: Added tests for openedx_yaml check

---
+++ tests/test_check_openedx_yaml.py
@@ -0,0 +1,94 @@
+import os
+import pytest
+
+from repo_health.check_openedx_yaml import (
+    check_oeps,
+    check_obsolete_fields,
+    check_release_maybe,
+    check_release_ref,
+    check_yaml_parsable,
+    module_dict_key,
+    fixture_openedx_yaml,
+    fixture_oeps,
+    fixture_parsed_data,
+    output_keys,
+    obsolete_fields,
+)
+
+
+def get_repo_path(repo_name):
+    tests_directory = os.path.dirname(__file__)
+    return f"{tests_directory}/fake_repos/{repo_name}"
+
+
+@pytest.mark.parametrize("repo_path, result", [
+    (get_repo_path('openedx_repo1'),'master'),
+    (get_repo_path('openedx_repo2'),''),
+])
+
+def test_check_release_ref(parsed_data, result):
+    all_results = {module_dict_key:{}}
+    check_release_ref(parsed_data, all_results)
+
+    assert all_results[module_dict_key]['release'] == result
+
+
+@pytest.mark.parametrize("repo_path, result", [
+    (get_repo_path('openedx_repo1'),True),
+    (get_repo_path('openedx_repo2'),False)
+])
+
+def test_check_release_maybe(parsed_data, result):
+    all_results = {module_dict_key:{}}
+    check_release_maybe(parsed_data, all_results)
+
+    assert all_results[module_dict_key]['release-maybe'] == result
+
+
+@pytest.mark.parametrize("repo_path, result", [
+    (get_repo_path('openedx_repo1'),True),
+    (get_repo_path('openedx_repo2'),True),
+    (get_repo_path('docs_repo'), False),
+])
+
+def test_check_yaml_parsable(openedx_yaml, result):
+    all_results = {module_dict_key:{}}
+    check_yaml_parsable(openedx_yaml, all_results)
+
+    assert all_results[module_dict_key]['parsable'] == result
+
+
+@pytest.mark.parametrize("repo_path, result_list", [
+    (get_repo_path('openedx_repo1'),{
+        "oep-2":True,
+        "oep-7":False,
+        "oep-18":True,
+        "oep-30":False
+    }),
+    (get_repo_path('openedx_repo2'),{
+        "oep-2":True,
+        "oep-7":True,
+        "oep-18":False,
+        "oep-30":False
+    })
+])
+
+def test_check_oeps(oeps, result_list):
+    all_results = {module_dict_key:{}}
+    check_oeps(oeps, all_results)
+
+    for key, desc in output_keys.items():
+        assert all_results[module_dict_key][key] == result_list[key]
+
+
+@pytest.mark.parametrize("repo_path, result_list", [
+    (get_repo_path('openedx_repo1'),['nick']),
+    (get_repo_path('openedx_repo2'),['nick', 'supporting-teams'])
+])
+
+def test_check_obsolete_fields(parsed_data, result_list):
+    all_results = {module_dict_key:{}}
+    check_obsolete_fields(parsed_data, all_results)
+
+    for field in all_results[module_dict_key]['obsolete_fields'].split():
+        assert field in result_list

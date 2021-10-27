3ff69d51024b0054456ca84f302bd0e0c49f3c17
Ned Batchelder <ned@edx.org>
2021-10-27 16:46:51 -0400

    style: correctly indent the test data, and update the style

--- tests/test_check_existence.py
+++ tests/test_check_existence.py
@@ -17,81 +17,84 @@ def get_repo_path(repo_name):
 
 
 @pytest.mark.parametrize("fake_repo, flag_list", [
-    ("kodegail",
-    {"openedx.yaml": False,
-    "Makefile": False,
-    "tox.ini":  False,
-    ".travis.yml":  False,
-    "README.rst":  False,
-    "CHANGELOG.rst":  False,
-    "pylintrc":  False,
-    "setup.cfg":  True,
-    "setup.py":  True,
-    ".coveragerc":  False,
-    ".editorconfig":  False,
-    ".pii_annotations.yml":  False,
-    ".gitignore":  False,
-    "package.json":  False,
+    ("kodegail", {
+        "openedx.yaml": False,
+        "Makefile": False,
+        "tox.ini": False,
+        ".travis.yml": False,
+        "README.rst": False,
+        "CHANGELOG.rst": False,
+        "pylintrc": False,
+        "setup.cfg": True,
+        "setup.py": True,
+        ".coveragerc": False,
+        ".editorconfig": False,
+        ".pii_annotations.yml": False,
+        ".gitignore": False,
+        "package.json": False,
     }),
-    ("python_js_repo", {"openedx.yaml": False,
-    "Makefile": False,
-    "tox.ini":  False,
-    ".travis.yml":  False,
-    "README.rst":  False,
-    "CHANGELOG.rst":  False,
-    "pylintrc":  False,
-    "setup.cfg":  False,
-    "setup.py":  False,
-    ".coveragerc":  False,
-    ".editorconfig":  False,
-    ".pii_annotations.yml":  False,
-    ".gitignore":  False,
-    "package.json":  True,
-    }),("just_setup_py",
-    {"openedx.yaml": False,
-    "Makefile": False,
-    "tox.ini":  False,
-    ".travis.yml":  False,
-    "README.rst":  False,
-    "CHANGELOG.rst":  False,
-    "pylintrc":  False,
-    "setup.cfg":  False,
-    "setup.py":  True,
-    ".coveragerc":  False,
-    ".editorconfig":  False,
-    ".pii_annotations.yml":  False,
-    ".gitignore":  False,
-    "package.json":  False,
-    })])
-
+    ("python_js_repo", {
+        "openedx.yaml": False,
+        "Makefile": False,
+        "tox.ini": False,
+        ".travis.yml": False,
+        "README.rst": False,
+        "CHANGELOG.rst": False,
+        "pylintrc": False,
+        "setup.cfg": False,
+        "setup.py": False,
+        ".coveragerc": False,
+        ".editorconfig": False,
+        ".pii_annotations.yml": False,
+        ".gitignore": False,
+        "package.json": True,
+    }),
+    ("just_setup_py", {
+        "openedx.yaml": False,
+        "Makefile": False,
+        "tox.ini": False,
+        ".travis.yml": False,
+        "README.rst": False,
+        "CHANGELOG.rst": False,
+        "pylintrc": False,
+        "setup.cfg": False,
+        "setup.py": True,
+        ".coveragerc": False,
+        ".editorconfig": False,
+        ".pii_annotations.yml": False,
+        ".gitignore": False,
+        "package.json": False,
+    })
+])
 def test_check_file_existence(fake_repo, flag_list):
-    repo_path = get_repo_path('fake_repos/'+ fake_repo)
-    all_results = {module_dict_key:{}}
+    repo_path = get_repo_path(f'fake_repos/{fake_repo}')
+    all_results = {module_dict_key: {}}
     check_file_existence(repo_path, all_results)
 
-    for file, desc in req_files.items():
+    for file in req_files.keys():
         assert all_results[module_dict_key][file] == flag_list[file]
 
 
 @pytest.mark.parametrize("fake_repo, flag_list", [
-    ("kodegail",{"requirements": False}),
-    ("python_js_repo", {"requirements": True})])
-
+    ("kodegail", {"requirements": False}),
+    ("python_js_repo", {"requirements": True}),
+])
 def test_check_dir_existence(fake_repo, flag_list):
-    repo_path = get_repo_path('fake_repos/'+ fake_repo)
-    all_results = {module_dict_key:{}}
+    repo_path = get_repo_path(f'fake_repos/{fake_repo}')
+    all_results = {module_dict_key: {}}
     check_dir_existence(repo_path, all_results)
 
-    for dir, desc in req_dirs.items():
+    for dir in req_dirs.keys():
         assert all_results[module_dict_key][dir] == flag_list[dir]
 
 
 @pytest.mark.parametrize("fake_repo, flag", [
     ("docs_repo", True),
     ("js_repo", True),
-    ("just_setup_cfg", False)])
+    ("just_setup_cfg", False),
+])
 def test_readme_existence(fake_repo, flag):
-    repo_path = get_repo_path('fake_repos/' + fake_repo)
+    repo_path = get_repo_path(f'fake_repos/{fake_repo}')
     all_results = {module_dict_key: {}}
     check_readme_existence(repo_path, all_results)
 

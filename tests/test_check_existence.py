e03b68aaa530b332783bb83f247c8a41054bb69b
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-03-14 17:46:48 +0500

    chore: Updating a check to see transifex configuration exists.

--- tests/test_check_existence.py
+++ tests/test_check_existence.py
@@ -34,6 +34,7 @@ def get_repo_path(repo_name):
         ".pii_annotations.yml": False,
         ".gitignore": False,
         "package.json": False,
+        "config": False,
     }),
     ("python_js_repo", {
         "openedx.yaml": False,
@@ -50,6 +51,7 @@ def get_repo_path(repo_name):
         ".pii_annotations.yml": False,
         ".gitignore": False,
         "package.json": True,
+        "config": True,
     }),
     ("just_setup_py", {
         "openedx.yaml": False,

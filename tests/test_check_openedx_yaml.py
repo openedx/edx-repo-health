5f2def01125519ddf0d1b080b22cb66148790715
Ned Batchelder <ned@edx.org>
2023-01-12 18:05:03 -0500

    style: more conventional spacing

--- tests/test_check_openedx_yaml.py
+++ tests/test_check_openedx_yaml.py
@@ -22,10 +22,9 @@ def get_repo_path(repo_name):
 
 
 @pytest.mark.parametrize("repo_path, result", [
-    (get_repo_path('openedx_repo1'),'master'),
-    (get_repo_path('openedx_repo2'),''),
+    (get_repo_path("openedx_repo1"), "master"),
+    (get_repo_path("openedx_repo2"), ""),
 ])
-
 def test_check_release_ref(parsed_data, result):
     all_results = {module_dict_key:{}}
     check_release_ref(parsed_data, all_results)
@@ -34,10 +33,9 @@ def test_check_release_ref(parsed_data, result):
 
 
 @pytest.mark.parametrize("repo_path, result", [
-    (get_repo_path('openedx_repo1'),True),
-    (get_repo_path('openedx_repo2'),False)
+    (get_repo_path("openedx_repo1"), True),
+    (get_repo_path("openedx_repo2"), False),
 ])
-
 def test_check_release_maybe(parsed_data, result):
     all_results = {module_dict_key:{}}
     check_release_maybe(parsed_data, all_results)
@@ -46,11 +44,10 @@ def test_check_release_maybe(parsed_data, result):
 
 
 @pytest.mark.parametrize("repo_path, result", [
-    (get_repo_path('openedx_repo1'),True),
-    (get_repo_path('openedx_repo2'),True),
-    (get_repo_path('docs_repo'), False),
+    (get_repo_path("openedx_repo1"), True),
+    (get_repo_path("openedx_repo2"), True),
+    (get_repo_path("docs_repo"), False),
 ])
-
 def test_check_yaml_parsable(openedx_yaml, result):
     all_results = {module_dict_key:{}}
     check_yaml_parsable(openedx_yaml, all_results)
@@ -59,20 +56,19 @@ def test_check_yaml_parsable(openedx_yaml, result):
 
 
 @pytest.mark.parametrize("repo_path, result_list", [
-    (get_repo_path('openedx_repo1'),{
+    (get_repo_path("openedx_repo1"), {
         "oep-2":True,
         "oep-7":False,
         "oep-18":True,
         "oep-30":False
     }),
-    (get_repo_path('openedx_repo2'),{
+    (get_repo_path("openedx_repo2"), {
         "oep-2":True,
         "oep-7":True,
         "oep-18":False,
         "oep-30":False
-    })
+    }),
 ])
-
 def test_check_oeps(oeps, result_list):
     all_results = {module_dict_key:{}}
     check_oeps(oeps, all_results)
@@ -82,10 +78,9 @@ def test_check_oeps(oeps, result_list):
 
 
 @pytest.mark.parametrize("repo_path, result_list", [
-    (get_repo_path('openedx_repo1'),['nick']),
-    (get_repo_path('openedx_repo2'),['nick', 'supporting-teams'])
+    (get_repo_path("openedx_repo1"), ["nick"]),
+    (get_repo_path("openedx_repo2"), ["nick", "supporting-teams"]),
 ])
-
 def test_check_obsolete_fields(parsed_data, result_list):
     all_results = {module_dict_key:{}}
     check_obsolete_fields(parsed_data, all_results)

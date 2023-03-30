aa961c10133d01335c25c4ab5b620ac2f1b605aa
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-03-30 17:46:49 +0500

    fix: ruamel.yaml package removed

--- repo_health/check_dependabot.py
+++ repo_health/check_dependabot.py
@@ -1,8 +1,8 @@
 """
 Checks to identify whether the Dependabot file exists and which ecosystems are covered under Dependabot
 """
-from ruamel.yaml import YAML
-
+from collections import OrderedDict
+import yaml
 import pytest
 from pytest_repo_health import add_key_to_metadata, health_metadata
 from repo_health import get_file_content
@@ -39,13 +39,14 @@ def check_has_ecosystems(dependabot_yml, all_results):
     """
     ecosystems = ["pip", "npm", "github-actions"]
     all_results[module_dict_key]["has_ecosystem"] = {}
-    yml_instance = YAML()
     for ecosystem in ecosystems:
         found = False
         if dependabot_yml:
             dependabot_elements = []
             with open(dependabot_path) as file_stream:
-                dependabot_elements = yml_instance.load(file_stream)
+                dependabot_elements = OrderedDict(
+                    yaml.safe_load(file_stream.read())
+                )
             dependabot_elements['updates'] = dependabot_elements.get('updates') or []
             for index in dependabot_elements['updates']:
                 if ecosystem == index.get('package-ecosystem'):

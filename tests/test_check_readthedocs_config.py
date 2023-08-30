9423a9d9b5da3d0440987244103e53e03203d462
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-08-30 15:40:46 +0500

    fix: 12 test cases added to test the readthedocs checks

--- tests/test_check_readthedocs_config.py
+++ tests/test_check_readthedocs_config.py
@@ -13,133 +13,190 @@ from repo_health.check_readthedocs_config import (
 )
 
 
-@pytest.fixture(name="readthedocs_config_details")
-def fixture_readthedocs_config_details(repo_path):
-    """Fixture returns the name and the content of readthedocs file"""
-    files_to_check = [
-        "readthedocs.yml",
-        "readthedocs.yaml",
-        ".readthedocs.yml",
-        ".readthedocs.yaml"
-    ]
-    for readthedocs_file in files_to_check:
-        file_content = get_file_content(os.path.join(repo_path, f"tests/fake_repos/readthedocs_samples/{readthedocs_file}"))
-        if file_content:
-            return {
-                "file_name": readthedocs_file,
-                "file_content": file_content
-            }
-    return {}
 
-
-def test_check_readthedocs_yml_exists(readthedocs_config_details):
+def test_check_readthedocs_yml_exists():
     """
     Test to check if readthedocs.yml file exists
     """
     all_results = {module_dict_key: {}}
     check_readthedocs_file_exists(
-        readthedocs_config_details,
+        {
+            "file_name": "readthedocs.yml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/readthedocs.yml")
+        },
         all_results
     )
 
     assert all_results[module_dict_key]['exists'] is True
 
 
-def test_check_readthedocs_yaml_exists(readthedocs_config_details):
-    """
-    Test to check if readthedocs.yaml file exists
-    """
-    all_results = {module_dict_key: {}}
-    check_readthedocs_file_exists(
-        readthedocs_config_details,
-        all_results
-    )
-
-    assert all_results[module_dict_key]['exists'] is True
-
-def test_check_readthedocs_dot_yml_exists(readthedocs_config_details):
+def test_check_dot_readthedocs_yml_exists():
     """
     Test to check if .readthedocs.yml file exists
     """
     all_results = {module_dict_key: {}}
     check_readthedocs_file_exists(
-        readthedocs_config_details,
+        {
+            "file_name": ".readthedocs.yml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/.readthedocs.yml")
+        },
         all_results
     )
 
     assert all_results[module_dict_key]['exists'] is True
 
 
-def test_check_readthedocs_dot_yaml_exists(readthedocs_config_details):
+def test_check_readthedocs_yaml_exists():
+    """
+    Test to check if readthedocs.yaml file exists
+    """
+    all_results = {module_dict_key: {}}
+    check_readthedocs_file_exists(
+        {
+            "file_name": "readthedocs.yaml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/readthedocs.yaml")
+        },
+        all_results
+    )
+
+    assert all_results[module_dict_key]['exists'] is True
+
+
+def test_check_dot_readthedocs_yaml_exists():
     """
     Test to check if .readthedocs.yaml file exists
     """
     all_results = {module_dict_key: {}}
     check_readthedocs_file_exists(
-        readthedocs_config_details,
+        {
+            "file_name": ".readthedocs.yaml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/.readthedocs.yaml")
+        },
         all_results
     )
 
     assert all_results[module_dict_key]['exists'] is True
 
 
-def test_check_readthedocs_yml_exists(readthedocs_config_details):
+def test_check_readthedocs_yml_file_name():
     """
-    Test to check if readthedocs.yml file exists
+    Test to check if readthedocs.yml file name
     """
     all_results = {module_dict_key: {}}
     check_readthedocs_file_name(
-        readthedocs_config_details,
+        {
+            "file_name": "readthedocs.yml"
+        },
         all_results
     )
 
     assert all_results[module_dict_key]['file_name'] == "readthedocs.yml"
 
 
-def test_check_readthedocs_yaml_exists(readthedocs_config_details):
+def test_check_dot_readthedocs_yml_file_name():
     """
-    Test to check if readthedocs.yaml file exists
-    """
-    all_results = {module_dict_key: {}}
-    check_readthedocs_file_name(
-        readthedocs_config_details,
-        all_results
-    )
-
-    assert all_results[module_dict_key]['file_name'] == "readthedocs.yaml"
-
-
-def test_check_dot_readthedocs_yml_exists(readthedocs_config_details):
-    """
-    Test to check if .readthedocs.yml file exists
+    Test to check if .readthedocs.yml file name
     """
     all_results = {module_dict_key: {}}
     check_readthedocs_file_name(
-        readthedocs_config_details,
+        {
+            "file_name": ".readthedocs.yml"
+        },
         all_results
     )
 
     assert all_results[module_dict_key]['file_name'] == ".readthedocs.yml"
 
 
-def test_check_dot_readthedocs_yaml_exists(readthedocs_config_details):
+def test_check_readthedocs_yaml_file_name():
     """
-    Test to check if .readthedocs.yaml file exists
+    Test to check if readthedocs.yaml file name
     """
     all_results = {module_dict_key: {}}
     check_readthedocs_file_name(
-        readthedocs_config_details,
+        {
+            "file_name": "readthedocs.yaml"
+        },
+        all_results
+    )
+
+    assert all_results[module_dict_key]['file_name'] == "readthedocs.yaml"
+
+
+def test_check_dot_readthedocs_yaml_file_name():
+    """
+    Test to check if .readthedocs.yaml file name
+    """
+    all_results = {module_dict_key: {}}
+    check_readthedocs_file_name(
+        {
+            "file_name": ".readthedocs.yaml"
+        },
         all_results
     )
 
     assert all_results[module_dict_key]['file_name'] == ".readthedocs.yaml"
 
 
-def test_check_readthedocs_version_is_1_or_2(readthedocs_config_details):
+def test_check_readthedocs_yml_version_is_1_or_2():
     """
-    Test to check if the file content has version 1 or 2
+    Test to check if the readthedocs.yml file content has version 1 or 2
     """
     all_results = {module_dict_key: {}}
-    check_readthedocs_file_version(readthedocs_config_details, all_results)
+    check_readthedocs_file_version(
+        {
+            "file_name": "readthedocs.yml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/readthedocs.yml")
+        },
+        all_results
+    )
+
+    assert all_results[module_dict_key]['version'] in [1,2]
+
+
+def test_check_dot_readthedocs_yml_version_is_1_or_2():
+    """
+    Test to check if the .readthedocs.yml file content has version 1 or 2
+    """
+    all_results = {module_dict_key: {}}
+    check_readthedocs_file_version(
+        {
+            "file_name": ".readthedocs.yml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/.readthedocs.yml")
+        },
+        all_results
+    )
+
+    assert all_results[module_dict_key]['version'] in [1,2]
+
+
+def test_check_readthedocs_yaml_version_is_1_or_2():
+    """
+    Test to check if the readthedocs.yaml file content has version 1 or 2
+    """
+    all_results = {module_dict_key: {}}
+    check_readthedocs_file_version(
+        {
+            "file_name": "readthedocs.yaml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/readthedocs.yaml")
+        },
+        all_results
+    )
+
+    assert all_results[module_dict_key]['version'] in [1,2]
+
+
+def test_check_dot_readthedocs_yaml_version_is_1_or_2():
+    """
+    Test to check if the .readthedocs.yaml file content has version 1 or 2
+    """
+    all_results = {module_dict_key: {}}
+    check_readthedocs_file_version(
+        {
+            "file_name": ".readthedocs.yaml",
+            "file_content": get_file_content(f"tests/fake_repos/readthedocs_samples/.readthedocs.yaml")
+        },
+        all_results
+    )
 
     assert all_results[module_dict_key]['version'] in [1,2]

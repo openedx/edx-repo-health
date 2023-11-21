2a3be31d5b5c7dc6cfb29ac4d1fe0b75c389b640
salman2013 <salman.nawaz@arbisoft.com>
2023-11-21 17:15:50 +0500

    chore: fix test case

--- tests/test_check_dependencies.py
+++ tests/test_check_dependencies.py
@@ -16,7 +16,7 @@ def test_python_js_repo_dependency_check():
 
     assert 'fs==2.0.18' in dependencies["pypi_all"]["list"]
     assert "react-redux" in dependencies["js"]["list"]
-    assert dependencies["count"] == 350
+    assert dependencies["count"] == 348
     assert dependencies["pypi_all"]["count"] == 298
     assert dependencies["github"]["count"] == 15
     assert dependencies["js.all"]["count"] == 10
